ea6ba5c520dad0356d0c876e4e1552d6294b5898
dav <victordav@qq.com>
2024-03-07 02:08:53 +0800

    2024-3-7

--- sbtest/t/src/main/java/com/sgugo/sbtest/nio/BufferTest1.java
+++ sbtest/t/src/main/java/com/sgugo/sbtest/nio/BufferTest1.java
@@ -1,26 +1,22 @@
 package com.sgugo.sbtest.nio;
 
-import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
-import java.util.Arrays;
 
 public class BufferTest1 {
     public static void main(String[] args) {
-        CharBuffer buffer = CharBuffer.allocate(20);
-        buffer.put("hello 世界");
+        // 1. 创建buffer
+        CharBuffer buffer = CharBuffer.allocate(5);
 
-        //反转读写模式
+        // 2. 创建字符数组（长度为7）
+        char[] data = {'夫','吾','罐','何','言','蛇','出'};
+
+        // 3. 将数组中的数据写入缓冲区
+        //buffer.put(data); //缓冲区无法储存全部数据，会抛出异常
+        //使len = buffer.remaining()，只储存缓冲区可容纳的数据量
+        buffer.put(data, 0, buffer.remaining());
+
+        // 4. 反转读写模式，读取缓冲区的数据
         buffer.flip();
-        System.out.println(buffer);
-        System.out.println(buffer.capacity());
-        System.out.println(buffer.remaining());
-        //定义数组，数组的大小不能超过实际的capacity大小
-        char [] dst = new char[buffer.remaining()];
-
-        //把缓冲区中的数据读到字符数组中
-        //批量传输时，大小总是固定的，如果没有指定传输的大小，意味着把数组填满
-        CharBuffer remaining = buffer.get(dst);
-
-        System.out.println(Arrays.toString(dst));
+        System.out.println(buffer); //夫吾罐何言
     }
 }
