c796466763c8dd4ae990199c68006ccfe189c377
victor <Aaron@gmail.com>
2024-03-22 18:04:06 +0800

    2024-3-22

---
+++ sbtest/jvm/src/main/java/com/sgugo/jvm/Test1.java
@@ -0,0 +1,10 @@
+package com.sgugo.jvm;
+
+import java.io.IOException;
+
+public class Test1 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("running...");
+        System.in.read();
+    }
+}
