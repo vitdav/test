d4ad1fc8b615b1db638ba779365c03da32390aa9
victor <Aaron@gmail.com>
2024-04-16 18:15:40 +0800

    2024-4-16

---
+++ uni-test/pages/index/index.vue
@@ -0,0 +1,16 @@
+<template>
+	<view>
+		<h1>首页</h1>
+		<navigator url="/pages/login/login">
+			<button type="primary">登陆</button>
+		</navigator>
+	</view>
+</template>
+
+<script setup>
+	
+</script>
+
+<style>
+	
+</style>
---
+++ uni-test/pages/login/login.vue
@@ -0,0 +1,20 @@
+<template>
+	<view>
+		<h1>登陆页面</h1>
+	</view>
+</template>
+
+<script setup>
+import {onLoad} from '@dcloudio/uni-app'
+
+onLoad(()=>{
+	console.log("进入登陆页面")
+	uni.$emit('login',{
+		msg:"登陆数据"
+	})
+})
+</script>
+
+<style lang="scss">
+
+</style>
