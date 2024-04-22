11e7d0a1440e508cb0d8bd61e3d472e38f761014
victor <Aaron@gmail.com>
2024-04-22 17:35:22 +0800

    2024-4-22

--- 前端学习/uni-test-vue3/pages/about/about.vue
+++ 前端学习/uni-test-vue3/pages/about/about.vue
@@ -6,27 +6,11 @@
 </template>
 
 <script setup>
-import { onLoad,onUnload } from "@dcloudio/uni-app"
-import {ref} from 'vue'
+const obj = uni.getLaunchOptionsSync();
+console.log(obj)
 
-const userinfo = ref({})
-onLoad(()=>{
-	console.log("about：开始监听login事件")
-	//监听login事件,事件触发后执行回调函数
-	uni.$on('login',(userinfo)=>{
-		console.log("about：login事件已触发")
-		userinfo.value = userinfo
-		console.log("about：用户"+userinfo.value.userName+"已登陆")
-	})
-})
-
-onUnload(()=>{
-	//移除监听事件
-	uni.$off('login')
-	console.log("about：login事件已移除")
-})
-
-
+const obj2 = uni.getEnterOptionsSync();
+console.log(obj)
 </script>
 
 <style>
