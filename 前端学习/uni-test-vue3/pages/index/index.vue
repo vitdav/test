dd66b01cb24936909a0c72397bcf00ea0eef672d
victor <Aaron@gmail.com>
2024-04-24 18:08:04 +0800

    2024-4-24

--- 前端学习/uni-test-vue3/pages/index/index.vue
+++ 前端学习/uni-test-vue3/pages/index/index.vue
@@ -1,21 +1,19 @@
 <template>
 <view>
-	<h1>首页</h1>
+	<map style="width: 750rpx; height: 900rpx;" 
+		:latitude="latitude" 
+		:longitude="longitude" 
+		>
+	</map>
 	
+	<h2>除了地图，还能显示其他额外控件</h2>
+	<button type="primary">这是一个按钮</button>
 </view>
 </template>
 
 
 <script setup>
-import {onLoad} from '@dcloudio/uni-app'
-onLoad(()=>{
-	console.log("hello")
-	uni.getLocation({
-		type:'wgs84',
-		success(res){
-			console.log("获取位置信息")
-			console.log(res)
-		}
-	})
-})
+import {ref} from 'vue'
+const latitude = ref(39.999)
+const longitude = ref( 116.39742)
 </script>
