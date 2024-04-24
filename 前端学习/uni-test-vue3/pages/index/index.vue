5da775146fff6dcd2f2ce4a141982e9192f0dde2
dav <victordav@qq.com>
2024-04-25 01:28:41 +0800

    2024-4-25

--- 前端学习/uni-test-vue3/pages/index/index.vue
+++ 前端学习/uni-test-vue3/pages/index/index.vue
@@ -1,19 +1,35 @@
+<!-- 基本使用-->
 <template>
 <view>
-	<map style="width: 750rpx; height: 900rpx;" 
-		:latitude="latitude" 
-		:longitude="longitude" 
-		>
-	</map>
-	
-	<h2>除了地图，还能显示其他额外控件</h2>
-	<button type="primary">这是一个按钮</button>
+  <map style="width: 750rpx; height: 900rpx;" 
+    :latitude="latitude" 
+    :longitude="longitude" 
+		:markers="covers"
+    >
+  </map>
 </view>
 </template>
 
-
 <script setup>
 import {ref} from 'vue'
-const latitude = ref(39.999)
-const longitude = ref( 116.39742)
+const latitude = ref(39.999) //定义显示的中心维度
+const longitude = ref(116.39742)//定义显示的中心精度
+//定义markers（标记点）
+const covers = ref([
+	{
+		id:1,
+		latitude: 39.999,
+		longitude: 116.39742,
+		height:20,
+		width:20,
+		iconPath: '/static/location/pile.png'
+	}, {
+		id:2,
+		latitude: 39.999,
+		longitude: 116.395,
+		height:20,
+		width:20,
+		iconPath: '/static/location/pile.png'
+	},
+])
 </script>
