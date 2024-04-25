826cfeb2275b99b8c033e8dcd895ab2f8a897a7d
victor <Aaron@gmail.com>
2024-04-25 18:19:49 +0800

    2024-4-25

--- 前端学习/uni-test-vue3/pages/index/index.vue
+++ 前端学习/uni-test-vue3/pages/index/index.vue
@@ -1,35 +1,22 @@
-<!-- 基本使用-->
 <template>
 <view>
-  <map style="width: 750rpx; height: 900rpx;" 
-    :latitude="latitude" 
-    :longitude="longitude" 
-		:markers="covers"
-    >
-  </map>
+  <button @click="openDebug">打开调试</button>
+	<button @click="closeDebug">关闭调试</button>
 </view>
 </template>
 
 <script setup>
 import {ref} from 'vue'
-const latitude = ref(39.999) //定义显示的中心维度
-const longitude = ref(116.39742)//定义显示的中心精度
-//定义markers（标记点）
-const covers = ref([
-	{
-		id:1,
-		latitude: 39.999,
-		longitude: 116.39742,
-		height:20,
-		width:20,
-		iconPath: '/static/location/pile.png'
-	}, {
-		id:2,
-		latitude: 39.999,
-		longitude: 116.395,
-		height:20,
-		width:20,
-		iconPath: '/static/location/pile.png'
-	},
-])
+function openDebug(){
+	//打开调试
+	uni.setEnableDebug({
+		enableDebug:true
+	})
+}
+
+function closeDebug(){
+	uni.setEnableDebug({
+		enableDebug:false
+	})
+}
 </script>
