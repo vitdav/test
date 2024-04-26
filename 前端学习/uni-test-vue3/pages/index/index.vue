f330ee875968e47ccf1d167195d26a00cafb1f22
victor <Aaron@gmail.com>
2024-04-26 18:12:28 +0800

    2024-4-26

--- 前端学习/uni-test-vue3/pages/index/index.vue
+++ 前端学习/uni-test-vue3/pages/index/index.vue
@@ -1,22 +1,25 @@
 <template>
 <view>
-  <button @click="openDebug">打开调试</button>
-	<button @click="closeDebug">关闭调试</button>
+  <button @click="saveImg">拍照保存</button>
 </view>
 </template>
 
 <script setup>
-import {ref} from 'vue'
-function openDebug(){
-	//打开调试
-	uni.setEnableDebug({
-		enableDebug:true
-	})
-}
 
-function closeDebug(){
-	uni.setEnableDebug({
-		enableDebug:false
+function saveImg(){
+	uni.chooseImage({
+		count:1,
+		//访问相机
+		sourceType:['camera'],
+		success(res){
+			uni.saveImageToPhotosAlbum({
+				filePath:res.tempFilePaths[0],
+				success(res){
+					console.log(res)
+					console.log("图片保存成功")
+				}
+			})
+		}
 	})
 }
 </script>
