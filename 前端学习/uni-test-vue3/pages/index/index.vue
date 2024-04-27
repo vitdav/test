77274071757ce4a4878e2cce994026368c4a05e1
victor <Aaron@gmail.com>
2024-04-27 18:02:58 +0800

    2024-4-27

--- 前端学习/uni-test-vue3/pages/index/index.vue
+++ 前端学习/uni-test-vue3/pages/index/index.vue
@@ -1,25 +1,21 @@
+<!--  -->
 <template>
 <view>
-  <button @click="saveImg">拍照保存</button>
+  <button @click="playVideo">播放背景音</button>
+	<button @click="stopVideo">停止背景音</button>
+	<button @click="pauseVideo">暂停背景音</button>
+	<button @click="mulPlay">二倍速播放</button>
+	
+	<!-- 创建一个audio组件，控制它播放录音 -->
+	<!-- <audio :src="voicePath" name="当前录音"></audio> -->
 </view>
 </template>
 
 <script setup>
+import {ref} from 'vue'
+//当前录音储存的临时地址
+const voicePath = ref('')
 
-function saveImg(){
-	uni.chooseImage({
-		count:1,
-		//访问相机
-		sourceType:['camera'],
-		success(res){
-			uni.saveImageToPhotosAlbum({
-				filePath:res.tempFilePaths[0],
-				success(res){
-					console.log(res)
-					console.log("图片保存成功")
-				}
-			})
-		}
-	})
-}
+
+
 </script>
