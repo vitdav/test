1fb60f00e87e1d9c6c833d721f9d790f4fb023d3
victor <Aaron@gmail.com>
2024-04-29 18:01:04 +0800

    2024-4-29

--- 前端学习/uni-test-vue3/pages/index/index.vue
+++ 前端学习/uni-test-vue3/pages/index/index.vue
@@ -1,42 +1,34 @@
 <template>
 <view>
-	<video :src="src" controls id="myVideo"
-		enable-danmu danmu-btn >
-	</video>
-  <button @click="playVideo">播放视频</button>
-	<button @click="pauseVideo">暂停视频</button>
-	<view>
-		<input type="text" placeholder="输入弹幕" @blur="inputBlur">
-	</view>
-	<button @click="sendDanmu">发送弹幕</button>
+
 </view>
 </template>
 
 <script setup>
-import {ref} from 'vue'
-const src = ref('https://media.w3.org/2010/05/sintel/trailer.mp4')
-const danmuValue = ref('')
+import {onReady,onShow} from '@dcloudio/uni-app'
 
-const videoContext = uni.createVideoContext('myVideo')
-function playVideo(){
-	console.log("播放视频")
-	videoContext.play()
-}
-
-function pauseVideo(){
-	console.log('暂停视频')
-	videoContext.pause()
-}
-
-function inputBlur(e){
-	danmuValue.value = e.target.value
-}
-
-function sendDanmu(){
-	console.log('发送弹幕')
-	videoContext.sendDanmu({
-		text: danmuValue.value,
+onReady(()=>{
+	uni.setNavigationBarTitle({
+		title: '动态标题'
 	})
-}
+	
+	uni.setNavigationBarColor({
+		frontColor: '#ffffff',
+		backgroundColor:'#4cd964',
+		animation:{
+			duration:200,
+			timingFunc:'easeIn'
+		}
+	})
+	
+	uni.showNavigationBarLoading()
+	
+	setTimeout(()=>{
+		uni.hideNavigationBarLoading()
+	},1000)
+})
 
+onShow(()=>{
+	uni.hideHomeButton()
+})
 </script>
