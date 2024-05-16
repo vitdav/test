8b62058693b7b6c4b5e837d4629807ab01f3456c
victor <Aaron@gmail.com>
2024-05-16 17:58:50 +0800

    2024-5-16

---
+++ 前端学习/青年帮新闻/pages/detail/detail.vue
@@ -0,0 +1,46 @@
+<script setup>
+	
+</script>
+
+<template>
+  <view class="detail">
+		<!-- 文章的标题 -->
+		<view class="title">
+			文章标题文章标题文章标题文章标题文章标题文章标题文章标题文章标题文章标题文章标题文章标题文章标
+		</view>
+		<!-- 文章的元信息 -->
+		<view class="info">
+			<view class="author">编辑：张三</view>
+			<view class="time">2022-02-22 22:22:22</view>
+		</view>
+		<!-- 文章的内容 -->
+		<view class="content">
+			文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容
+		</view>
+  </view>
+</template>   
+
+<style lang="scss" scoped>
+.detail{
+	paddign:30rpx;
+	// 标题样式
+	.title{
+		font-size: 46rpx;
+		color: #333;
+	}
+	// 元信息样式
+	.info{
+		background:#f6f6f6;
+		padding:20rpx;
+		font-size:25rpx;
+		color:#666;
+		display: flex;
+		justify-content:space-between;
+		margin:40rpx 0;
+	}
+	//文章内容
+	.content{
+		padding-bottom:50rpx;
+	}
+}
+</style>
--- 前端学习/青年帮新闻/pages/index/index.vue
+++ 前端学习/青年帮新闻/pages/index/index.vue
@@ -1,29 +1,95 @@
 <script setup>
 import {ref} from 'vue'
+import {onLoad,onReachBottom} from '@dcloudio/uni-app'
 //设置导航条选项被选中时的下标
 const navIndex = ref(0)
-//导航条选项的点击事件：点击时设置该选项高亮显示
-function clickNav(index){
+//导航条选项的点击事件
+//点击时设置该选项高亮显示，并切换新闻列表的分类
+function clickNav(index, id){
 	navIndex.value = index;
+	//切换分类时，需要先将页码和新闻列表数据进行初始化
+	currentPage.value = 1;
+	newsArr.value = [];
+	getNewsData(id);
 }
+
+function goDetail(){
+	uni.navigateTo({
+		url:"/pages/detail/detail"
+	})
+}
+
+//获取页面数据
+const navArr = ref([]) //导航条
+const newsArr = ref([]) //新闻列表
+onLoad(()=>{
+	//获取导航栏数据
+	getNavData()
+	//获取新闻列表数据
+	getNewsData()
+})
+
+// 获取菜单栏数据
+function getNavData(){
+	uni.request({
+		url:"https://ku.qingnian8.com/dataApi/news/navlist.php",
+		success(res){
+			navArr.value = res.data
+			console.log(res)
+		}
+	})
+}
+
+// 获取新闻列表数据
+const currentPage = ref(1) //当前加载的新闻列表页面
+function getNewsData(id=50){
+	uni.request({
+		url:"https://ku.qingnian8.com/dataApi/news/newslist.php",
+		data:{
+			num:8, //显示的新闻数量，默认是8
+			cid:id,//新闻所属的分类id
+			page:currentPage.value,
+		},
+		success(res){
+			console.log(res)
+			// newsArr.value = res.data
+			//为了进行触底加载，需要将上一页的数据和新数据进行拼接
+			newsArr.value = [...newsArr.value,...res.data]
+		}
+	})
+}
+
+// 设置触底加载
+onReachBottom(()=>{
+	console.log("到底部了")
+	//使当前页码+1
+	currentPage.value++
+	//使用新的页面，再次获取新闻列表数据
+	getNewsData()
+})
 </script>
 <template>
 	<view class="home">
-		<!-- 横向的滚动条菜单 -->
+		<!-- 横向的导航条菜单 -->
 		<scroll-view scroll-x class="navscroll">
 			<view class="item" 
-				v-for="(item,index) in 10"
+				v-for="(item,index) in navArr" :key="item.id"
 				:class="index==navIndex?'active':''"
-				
-				@click="clickNav(index)"
-			>分类</view>
+				@click="clickNav(index,item.id)"
+			>{{item.classname}}</view>
 		</scroll-view>
 		
 		<!-- 内容主题：新闻列表 -->
 		<view class="content">
 			<!-- 通过newsbox组件循环显示新闻项 -->
-			<view class="row" v-for="item in 10">
-				<newsbox></newsbox>
+			<view class="row" v-for="item in newsArr" :key="item.id">
+				<newsbox 
+				:item="item"
+				@click.native="goDetail"></newsbox>
+			</view>
+			<!-- 当某分类下没有新闻时，进行空白页处理 -->
+			<view class="nodata" v-if="!newsArr.length">
+				<image src="../../static/icon/nodata.png" mode=""></image>
 			</view>
 		</view>
 	</view>
@@ -59,12 +125,20 @@ function clickNav(index){
 //设置新闻列表的样式
 .content{
 	padding: 30rpx;
+	//滚动条设置fixed布局后，会遮挡一部分content的内容，因此需要设置padding-top
+	padding-top:100rpx; 
 	.row{
 		//每个新闻间显示一条虚线分割
 		border-bottom: 1px dotted #efefef;
 		padding: 20rpx 0;
-		//滚动条设置fixed布局后，会遮挡一部分content的内容，因此需要设置padding-top
-		padding-top:100rpx; 
+	}
+}
+// 空白页处理的样式
+.nodata{
+	display:flex;
+	justify-content: center;
+	image{
+		width:360rpx;
 	}
 }
 </style>
